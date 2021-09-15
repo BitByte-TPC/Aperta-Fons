3b6791eeac5aab586e499b56474abc96ef393f10
theanmolsharma <65589791+theanmolsharma@users.noreply.github.com>
2021-09-15 13:20:31 +0530

    update: add assets like fonts, images etc.

---
+++ src/views/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+  <Login />
+</template>
+
+<script>
+import Login from "@/components/Login";
+export default {
+  name: "Auth",
+  components: {Login}
+}
+</script>
+
+<style scoped>
+
+</style>
